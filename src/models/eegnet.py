c95c3f493ed1973251bf346cca84be512491a971
fiorella <fmilla1000@gmail.com>
2026-07-05 15:14:51 +0200

    added 5 fold ensemble, improves accuracy

--- src/models/eegnet.py
+++ src/models/eegnet.py
@@ -7,9 +7,11 @@ from sklearn.model_selection import train_test_split
 from src.models.EEGModels import EEGNet
 from src.utils.paths import EEGNET_MODEL_DIR
 
+from sklearn.model_selection import StratifiedKFold
+
 # epoch = trial aaaa (not in eegnet context)
 
-def set_seed(seed=42):
+def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
     tf.keras.utils.set_random_seed(seed)
@@ -22,20 +24,7 @@ def set_seed(seed=42):
 def get_eegnet_model_path(subject):
     return EEGNET_MODEL_DIR / f"A{subject:02d}_eegnet.keras"
 
-
-def train_or_load_eegnet(subject, X_train, y_train):
-    set_seed(42 + subject)
-
-    model_path = get_eegnet_model_path(subject)
-
-    n_classes = 2
-    n_channels = X_train.shape[1]
-    n_samples = X_train.shape[2]
-
-    if model_path.exists():
-        model = tf.keras.models.load_model(model_path)
-        return model
-
+def create_eegnet_model(n_classes, n_channels, n_samples):
     model = EEGNet(
         nb_classes=n_classes,
         Chans=n_channels,
@@ -53,35 +42,79 @@ def train_or_load_eegnet(subject, X_train, y_train):
         optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), # paper adam
         metrics=["accuracy"]
     )
+    return model
 
-    early_stopping = tf.keras.callbacks.EarlyStopping(
-        monitor="val_loss",
-        patience=20,# logic it says, some rule to avoid stopping too early because of small random
-        restore_best_weights=True
+def get_eegnet_fold_model_path(subject, fold, base_seed):
+    return EEGNET_MODEL_DIR / f"A{subject:02d}_eegnet_kfold_seed{base_seed}_fold{fold}.keras"
+
+
+def train_or_load_eegnet(subject, X_train, y_train):
+    base_seed = 42
+    seed = base_seed + subject
+    set_seed(seed)
+
+    model_path = get_eegnet_model_path(subject)
+
+    n_classes = 2
+    n_channels = X_train.shape[1]
+    n_samples = X_train.shape[2]
+
+    n_folds = 5
+
+    skf = StratifiedKFold(
+        n_splits=n_folds,
+        shuffle=True,
+        random_state=seed
     )
 
-    X_tr, X_val, y_tr, y_val = train_test_split(
-        X_train,
-        y_train,
-        test_size=0.2,
-        stratify=y_train,
-        random_state=42 + subject,
-        shuffle=True
-    )
+    models = []
 
-    y_tr_cat = tf.keras.utils.to_categorical(y_tr, num_classes=n_classes)
-    y_val_cat = tf.keras.utils.to_categorical(y_val, num_classes=n_classes)
+    for fold, (train_idx, val_idx) in enumerate(skf.split(X_train, y_train), start=1):
+        model_path = get_eegnet_fold_model_path(subject, fold, base_seed)
 
-    model.fit(
-        X_tr,
-        y_tr_cat,
-        epochs=500,
-        batch_size=16,
-        validation_data=(X_val, y_val_cat),
-        callbacks=[early_stopping],
-        verbose=0
-    )
-    
+        if model_path.exists():
+            model = tf.keras.models.load_model(model_path)
+            models.append(model)
+            continue
+
+        print(f"Training A{subject:02d} EEGNet fold {fold}/{n_folds}")
+
+        set_seed(seed + fold)
+
+        X_tr = X_train[train_idx]
+        X_val = X_train[val_idx]
+        y_tr = y_train[train_idx]
+        y_val = y_train[val_idx]
+
+        y_tr_cat = tf.keras.utils.to_categorical(y_tr, num_classes=n_classes)
+        y_val_cat = tf.keras.utils.to_categorical(y_val, num_classes=n_classes)
+
+        model = create_eegnet_model(
+            n_classes,
+            n_channels,
+            n_samples
+        )
+
+        early_stopping = tf.keras.callbacks.EarlyStopping(
+            monitor="val_loss",
+            patience=20,
+            restore_best_weights=True
+        )
+
+        model.fit(
+            X_tr,
+            y_tr_cat,
+            epochs=500,
+            batch_size=16,
+            validation_data=(X_val, y_val_cat),
+            callbacks=[early_stopping],
+            verbose=0
+        )
+
+        model.save(model_path)
+
+        models.append(model)
+
     # y_train_cat = tf.keras.utils.to_categorical(y_train, num_classes=n_classes) # into categorical/vectors
     
     # model.fit(
@@ -94,13 +127,20 @@ def train_or_load_eegnet(subject, X_train, y_train):
     #     verbose=0
     # )
 
-    model.save(model_path)
-
-    return model
+    return models
 
 
-def predict_eegnet(model, X_eval):
-    probabilities = model.predict(X_eval, verbose=0)
-    y_pred = np.argmax(probabilities, axis=1)
+def predict_eegnet(models, X_eval):
+    if not isinstance(models, list):
+        models = [models]
+
+    probabilities = []
+
+    for model in models:
+        fold_probabilities = model.predict(X_eval, verbose=0)
+        probabilities.append(fold_probabilities)
+
+    mean_probabilities = np.mean(probabilities, axis=0)
+    y_pred = np.argmax(mean_probabilities, axis=1)
 
     return y_pred

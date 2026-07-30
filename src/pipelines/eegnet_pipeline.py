b798492a952457be24d9f905b7c33e1cf744f7be
fiorella <fmilla1000@gmail.com>
2026-07-30 19:45:37 +0200

    removed shap comments in eegnet_pipeline

--- src/pipelines/eegnet_pipeline.py
+++ src/pipelines/eegnet_pipeline.py
@@ -39,64 +39,6 @@ def evaluate_eegnet_for_subject(
         y_train,
     )
 
-    # rng = np.random.default_rng(42)
-    # background_idx = rng.choice(len(X_train), size=50, replace=False)
-
-    # background = X_train[background_idx].astype(np.float32)
-
-    # -------- choose class to explain --------
-    # class_index = 0  # 0 = left hand, 1 = right hand
-    # class_name = "Left hand"
-
-    # Predict labels for evaluation trials
-    # y_pred_probs = model.predict(X_eval)
-    # y_pred = np.argmax(y_pred_probs, axis=1)
-
-    # Use only correctly classified trials of the selected class
-    # correct_class_mask = (y_eval == class_index) & (y_pred == class_index)
-
-    # X_explain = X_eval[correct_class_mask].astype(np.float32)
-
-    # print(f"Correct {class_name} trials:", X_explain.shape)
-
-    # -------- compute SHAP --------
-    # explainer = shap.GradientExplainer(model, background)
-
-    # shap_values = explainer.shap_values(X_explain)
-
-    # print("SHAP computed")
-    # print(type(shap_values))
-    # print("shap_values shape:", shap_values.shape)
-
-    # Take SHAP values for the selected output class
-    # sv = shap_values[..., class_index]
-
-    # print("sv shape before squeeze:", sv.shape)
-
-    # sv = sv.squeeze(-1)
-
-    # print("sv shape after squeeze:", sv.shape)
-
-    # channel_names = [
-    #     "Fz",
-    #     "FC3", "FC1", "FCz", "FC2", "FC4",
-    #     "C5", "C3", "C1", "Cz", "C2", "C4", "C6",
-    #     "CP3", "CP1", "CPz", "CP2", "CP4",
-    #     "P1", "Pz", "P2", "POz"
-    # ]
-
-    # channel_importance = np.mean(np.abs(sv), axis=(0, 2))
-
-    # print("channel_importance shape:", channel_importance.shape)
-
-    # plt.figure(figsize=(10, 4))
-    # plt.bar(channel_names, channel_importance)
-    # plt.xticks(rotation=90)
-    # plt.ylabel("Mean absolute SHAP value")
-    # plt.title(f"EEGNet SHAP Channel Importance - Correct {class_name}")
-    # plt.tight_layout()
-    # plt.show()
-
     y_pred = predict_eegnet(
         models,
         X_eval,
